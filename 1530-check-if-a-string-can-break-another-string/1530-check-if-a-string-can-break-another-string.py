05bcf4e62e3b4da53bb12cf10d9c44d8abeb6307
Swaminathan K <ghy222624@gmail.com>
2025-08-23 18:41:17 +0530

    Time: 99 ms (51.87%), Space: 20.2 MB (28.04%) - LeetHub

--- 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.py
+++ 1530-check-if-a-string-can-break-another-string/1530-check-if-a-string-can-break-another-string.py
@@ -1,11 +1,3 @@
 class Solution:
-    def checkIfCanBreak(self, s1: str, s2: str) -> bool:
-        s1 = sorted(s1)
-        s2 = sorted(s2)
-        c = b = True
-        for i in range(len(s1)):
-            if s1[i] < s2[i]:
-                c = False
-            if s2[i] < s1[i]:
-                b = False
-        return c or b
+    def checkIfCanBreak(self, s: str, t: str) -> bool:
+        return all(map(le,*sorted(map(sorted,(s,t)))))
