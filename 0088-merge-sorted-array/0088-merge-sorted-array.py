5514e91f0f357efb04468e4c86d32ec5c8182311
Swaminathan K <ghy222624@gmail.com>
2025-08-30 14:31:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,9 +1,6 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        if m == 0 and n == 1:
-            nums1[:] = nums2
-            return
-        elif m == 1 and n == 0:
-            return
-        else:
-            nums1[:] = sorted(nums1[:m] + nums2[:n])
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
